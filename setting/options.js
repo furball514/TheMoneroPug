db91081f77cf8e8d2ac3dc20105aad3f3df38957
Karthik Nallasivan <karthik.nallasivan@gmail.com>
2017-03-28 15:22:54 +0530

    Update options.js

--- setting/options.js
+++ setting/options.js
@@ -1,1 +1,27 @@
-.
+$(document).ready(function(){
+  var setting = '';
+  var value;
+  $('#topic').change(function(){
+    value = $('#topic').val();
+  });
+	$('#btc').click(function(){
+   setting = 'btc';
+  });
+  $('#rxmr').click(function(){
+   setting = 'rxmr';
+  });
+  $('#rxmrtrader').click(function(){
+   setting = 'rxmrtrader';
+  });
+  $('#stack').click(function(){
+   setting = 'stack';
+  });
+
+
+  $('button').click(function(){
+    console.log(setting);
+    console.log(value);
+    localStorage.mySetting = setting;
+    localStorage.id = value;
+  });
+});

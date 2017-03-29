50d179eb41820cfe88574dd5e4f261923e22c8e2
Karthik Nallasivan <karthik.nallasivan@gmail.com>
2017-03-29 13:57:46 +0530

    Update popup.js

--- main/popup.js
+++ main/popup.js
@@ -124,4 +124,12 @@ $(document).ready(function() {
       stacknews();
       break;
   }
-});
+  $('').click(function() {
+  if (chrome.runtime.openOptionsPage) {
+    // New way to open options pages, if supported (Chrome 42+).
+    chrome.runtime.openOptionsPage();
+  } else {
+    // Reasonable fallback.
+    window.open(chrome.runtime.getURL('/setting/options.html'));
+  }
+});});
